b6d5b14d48a28daa1f8427ca9636ffcf79f2ac14
bennettval <benhudvalentine@gmail.com>
2018-02-27 13:51:50 -0600

    Data to add to DB for basic information like Category and Class

---
+++ BasicDBData.sql
@@ -0,0 +1,30 @@
+-- Basic Table data for Categories, Class, and School needs to be added
+
+-- Category Table
+INSERT INTO Category(0100, "Behavioral and Social Science");
+INSERT INTO Category(0200, "Biochemistry");
+INSERT INTO Category(0300, "Inorganic Chemistry");
+INSERT INTO Category(0400, "Organic Chemistry");
+INSERT INTO Category(0500, "Earth and Environmental Sciences");
+INSERT INTO Category(0600, "Animal Sciences");
+INSERT INTO Category(0700, "Medicine and Health");
+INSERT INTO Category(0800, "Microbiology");
+INSERT INTO Category(0900, "Physics and Astronomy");
+INSERT INTO Category(1000, "Engineering");
+INSERT INTO Category(1100, "Computer Science and Math");
+INSERT INTO Category(1200, "Robotics and Intelligent Machines");
+INSERT INTO Category(1300, "Botany");
+
+-- Class Table
+insert into Class(1, 1);
+insert into Class(1, 2);
+insert into Class(1, 3);
+insert into Class(2, 4);
+insert into Class(2, 5);
+insert into Class(2, 6);
+insert into Class(3, 7);
+insert into Class(3, 8);
+insert into Class(4, 9);
+insert into Class(4, 10);
+insert into Class(5, 11);
+insert into Class(5, 12);
