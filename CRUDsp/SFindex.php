e7ed665fdfd0fb9c55923e2e0ce07859e40cd57a
bennettval <benhudvalentine@gmail.com>
2018-03-14 20:04:16 -0600

    Started making home page

--- CRUDsp/SFindex.php
+++ CRUDsp/SFindex.php
@@ -3,9 +3,52 @@
 
 <?php 
 	require_once("included_functions.php");
-	new_header();
 	$mysql = db_connection();
-	
+	//new_header("Home")	
  ?>
  	
+ 	 <head>
+		 <meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- metalink for bootstrap4 -->
+		 <link rel="stylesheet" href="css/bootstrap.css"/>
+		 <link rel="stylesheet" href="css/bootstrap-grid.css"/>
+		
+	</head>
+	<body>
+
+	<!-- content goes here -->
+	<nav class="navbar navbar-light" role="navigation">
+		 <div class="container-fluid">
+		 <div class="navbar-header">
+		 	<a class="navbar-brand" href="SFindex.php">MSEF Region 7 Science and Engineering Fairs at the University of Mississippi</a>
+		 </div>
+    	
+		 </div>
+	</nav>
+		
+	<nav class="navbar navbar-dark" style="background-color: #003366">
+		 <div class="container-fluid">
+		 <div class="navbar-header">
+		 	<span class="navbar-brand mb-0 h1">Home</span>
+		 </div>
+				
+		 </div>
+	</nav>
+
+<?php 
+
+	$query = "Select * from Fair";
+
+?>
+
+	<!-- Bootstrap4 scripts (jquery and popper are CDN because not downloaded) -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+
  </html>
