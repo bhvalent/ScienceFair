395c6e0d82fc2e29f64cbfd9213043f58996d759
bennettval <benhudvalentine@gmail.com>
2018-03-16 15:40:39 -0600

    made home page for fair

---
+++ CRUDsp/FairHome.php
@@ -0,0 +1,61 @@
+<?php 
+	require_once("session.php");
+?>
+
+<DOCTYPE! html>
+<html lang="en">
+
+<?php 
+	require_once("included_functions.php");
+	$mysqli = db_connection();
+	if (($output = message()) !== null) {
+		echo $output;
+	}
+	if (isset($_GET['id']) && $_GET['id'] != "") {
+		$ID = $_GET['id'];
+		$query = "Select * from Fair where FairID = ".$ID;
+
+		$result = $mysqli->query($query);
+		if ($result && $result->num_rows > 0) {
+			$row = $result->fetch_assoc();
+			new_header($row['FairName']." ".$row['Year']);
+		}
+	}	
+ ?>
+
+ <body>
+
+ 	<div class='container'>
+ 		<br>
+ 		<div class='row justify-content-center'>
+ 			<div class='col-6'>
+ 				<div class='card text-center'>
+ 					<div class='card-body'>
+ 						<h3 class='card-title'>Registrants</h3>
+ 						<p class='card-text'>View Registrants by Name, School, or Category</p>
+ 						<a href='#' class='btn btn-primary'>By Name</a>
+ 						<a href='#' class='btn btn-primary'>By School</a>
+ 						<a href='#' class='btn btn-primary'>By Category</a>
+ 						<a href='#' class='btn btn-primary'>By Class</a>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<br>
+ 		<!-- Need to edit to fit with judges -->
+ 		<div class='row justify-content-center'>
+ 			<div class='col-6'>
+ 				<div class='card text-center'>
+ 					<div class='card-body'>
+ 						<h3 class='card-title'>Judges</h3>
+ 						<p class='card-text'>View Judges by Name, School, or Category</p>
+ 						<a href='#' class='btn btn-primary'>By Name</a>
+ 						<a href='#' class='btn btn-primary'>By Category</a>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </body>
+
+ </html>
