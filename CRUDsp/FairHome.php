86e939a746ae435ce8834c8e1c775e3818178893
bennettval <benhudvalentine@gmail.com>
2018-03-18 15:33:41 -0500

    add links to readStudents.php

--- CRUDsp/FairHome.php
+++ CRUDsp/FairHome.php
@@ -20,7 +20,7 @@
 			$row = $result->fetch_assoc();
 			new_header($row['FairName']." ".$row['Year']);
 		}
-	}	
+	} 
  ?>
 
  <body>
@@ -31,12 +31,19 @@
  			<div class='col-6'>
  				<div class='card text-center'>
  					<div class='card-body'>
- 						<h3 class='card-title'>Registrants</h3>
- 						<p class='card-text'>View Registrants by Name, School, or Category</p>
- 						<a href='#' class='btn btn-primary'>By Name</a>
- 						<a href='#' class='btn btn-primary'>By School</a>
- 						<a href='#' class='btn btn-primary'>By Category</a>
- 						<a href='#' class='btn btn-primary'>By Class</a>
+ 						<h3 class='card-title'>Students</h3>
+ 						<p class='card-text'>View Students by Name, School, Category, or Class</p>
+
+ 						<?php
+ 						echo "<a href='readStudents.php?id=".urldecode($ID)."&type=1' class='btn btn-primary'>By Name</a>";
+ 						echo " ";
+ 						echo "<a href='readStudents.php?id=".urldecode($ID)."&type=2' class='btn btn-primary'>By School</a>";
+ 						echo " ";
+ 						echo "<a href='readStudents.php?id=".urldecode($ID)."&type=3' class='btn btn-primary'>By Category</a>";
+ 						echo " ";
+ 						echo "<a href='readStudents.php?id=".urldecode($ID)."&type=4' class='btn btn-primary'>By Class</a>";
+ 						?>
+
  					</div>
  				</div>
  			</div>
