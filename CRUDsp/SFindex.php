953a596d8524e0a2d00befd8bc370522c89e75bd
bennettval <benhudvalentine@gmail.com>
2018-04-02 21:23:42 -0500

    fixed links

--- CRUDsp/SFindex.php
+++ CRUDsp/SFindex.php
@@ -5,13 +5,7 @@
 <DOCTYPE! html>
 <html lang="en">
 
-<?php 
-	require_once("included_functions.php");
-	$mysqli = db_connection();
-	if (($output = message()) !== null) {
-		echo $output;
-	}	
- ?>
+
  	
  	 <head>
 		 <meta charset="utf-8">
@@ -23,6 +17,15 @@
 		
 	</head>
 	<body>
+
+	<?php 
+		require_once("included_functions.php");
+		$mysqli = db_connection();
+		if (($output = message()) !== null) {
+			echo $output;
+		}
+		
+ 	?>	
 
 	<!-- content goes here -->
 	<nav class="navbar navbar-light" role="navigation">
@@ -42,6 +45,16 @@
 				
 		 </div>
 	</nav>
+
+	<!-- Bootstrap4 scripts (jquery and popper are CDN because not downloaded) -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+
+
 
 <?php 
 
@@ -93,12 +106,6 @@
 
 ?>
 
-	<!-- Bootstrap4 scripts (jquery and popper are CDN because not downloaded) -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-
-	<script src="js/bootstrap.min.js"></script>
-	</body>
+	
 
  </html>
