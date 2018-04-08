6f438cd5cd446e73465c0cc47efb9bad39cb9911
bennettval <benhudvalentine@gmail.com>
2018-04-07 20:29:30 -0500

    fixed spacing

--- CRUDsp/addFair.php
+++ CRUDsp/addFair.php
@@ -107,7 +107,7 @@
 	}
 
 ?>
-
+	<br /><br /><br />
 
 	<!-- Bootstrap4 scripts (jquery and popper are CDN because not downloaded) -->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
