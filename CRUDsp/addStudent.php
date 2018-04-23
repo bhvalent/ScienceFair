906df6c50a7d4448c3f26d72eeb00a7ffc45eabd
bennettval <benhudvalentine@gmail.com>
2018-04-23 10:22:03 -0500

    added team member funcionality

--- CRUDsp/addStudent.php
+++ CRUDsp/addStudent.php
@@ -1,5 +1,5 @@
 <?php 
-	  require_once("session.php");
+      require_once("session.php");
 ?>
 
 <DOCTYPE! html>
@@ -7,17 +7,20 @@
 
 <?php 
     require_once("included_functions.php");
-	  $mysqli = db_connection();
+      $mysqli = db_connection();
     if (($output = message()) !== null) {
         echo $output;
-	  }
-		if (isset($_POST["submit"])) {
-        //echo "<h2>Got the submit</h2>";
-		    if ( (isset($_POST["projTitle"]) && $_POST["projTitle"] !== "") && (isset($_POST["fname"]) && $_POST["fname"] !== "") && (isset($_POST["lname"]) && $_POST["lname"] !== "") && (isset($_POST["age"]) && $_POST["age"] !== "") && (isset($_POST["genderRadioOptions"]) && $_POST["genderRadioOptions"] !== "") && (isset($_POST["city"]) && $_POST["city"] !== "") && (isset($_POST["state"]) && $_POST["state"] !== "") && (isset($_POST["zip"]) && $_POST["zip"] !== "") && (isset($_POST["school"]) && $_POST["school"] !== "") && (isset($_POST["adultSponsor"]) && $_POST["adultSponsor"] !== "") ) {
+    }
+    if (isset($_POST['submit'])) {
+    
+        if ( (isset($_POST["projTitle"]) && $_POST["projTitle"] !== "") && (isset($_POST["fname"]) && $_POST["fname"] !== "") && (isset($_POST["lname"]) && $_POST["lname"] !== "") && (isset($_POST["age"]) && $_POST["age"] !== "") && (isset($_POST["genderRadioOptions"]) && $_POST["genderRadioOptions"] !== "") && (isset($_POST["city"]) && $_POST["city"] !== "") && (isset($_POST["state"]) && $_POST["state"] !== "") && (isset($_POST["zip"]) && $_POST["zip"] !== "") && (isset($_POST["school"]) && $_POST["school"] !== "") && (isset($_POST["adultSponsor"]) && $_POST["adultSponsor"] !== "") && (isset($_POST["class"]) && $_POST["class"] !== "") ) {
 
-          $ID = $_GET['id'];
+            $ID = $_GET['id'];
 
-            //echo "<h2>Recieved all the information</h2>";
+                
+            echo $_POST['fname']." ".$_POST['lname'];
+
+                
             $query = "Insert into Registration ";
             $query .= "(LName, FName, ProjTitle, Age, Gender, City, State, Zip, AdultSponsor, FKFairID, FKSchoolID, FKCategoryID, FKClassID) ";
             $query .= "VALUES(";
@@ -39,16 +42,38 @@
 
             if($result) {
 
-                $_SESSION["message"] = $_POST["fname"]." ".$_POST["lname"]." has been added";
-                header("Location: FairHome.php?id=".$ID);
-                exit;
+                $query2 = "Select RegistrationID from Registration where ";
+                $query2 .= "LName = '".$_POST['lname']."' and ";
+                $query2 .= "FName = '".$_POST['fname']."' and ";
+                $query2 .= "ProjTitle = '".$_POST['projTitle']."' and ";
+                $query2 .= "AdultSponsor = '".$_POST['adultSponsor']."' and ";
+                $query2 .= "FKFairID = ".$ID;
 
+                $result2 = $mysqli->query($query2);
+                if ($result2 && $result2->num_rows > 0) {
+                    
+                    if (($row2 = $result2->fetch_assoc()) !== null) {
+                        $_SESSION["message"] = "".$_POST["fname"]." ".$_POST["lname"]." has been added";
+                        header("Location: addMoreTM.php?id=".$ID."&rid=".$row2['RegistrationID']);
+                        exit;
+                    }
+                    
+                } else {
+                    $_SESSION["message"] = "".$_POST["fname"]." ".$_POST["lname"]." has been added";
+                    header("Location: FairHome.php?id=".$ID);
+                    exit;
+                }
+
+
+
+                
             } else {
 
                 $_SESSION["message"] = "Error! Could not add ".$_POST["fname"]." ".$_POST["lname"];
                 header("Location: FairHome.php?id=".$ID);
                 exit;
             }
+            
 
         } else {
             $_SESSION["message"] = "Unable to add student. Fill in all information!";
@@ -56,8 +81,8 @@
             exit;
         }
 
-      
-		} else {
+  
+    } else {
         if (isset($_GET['id']) && $_GET['id'] != "") {
             $ID = $_GET['id'];
             $query = "Select * from Fair where FairID = ".$ID;
@@ -68,77 +93,79 @@
                 new_header($row['FairName']." ".$row['Year'], $ID);
                 $fairNameYear = $row['FairName']." ".$row['Year'];
             
-			          echo "<br /><br />";
-			          echo "<div class='row justify-content-center'";
-			          echo "<head>";
-			          echo "<h3 class='d-none d-md-block'>Add Student: <i>".$fairNameYear."</i></h3>";
+                echo "<br /><br />";
+                echo "<div class='row justify-content-center'";
+                echo "<head>";
+                echo "<h3 class='d-none d-md-block'>Add Student: <i>".$fairNameYear."</i></h3>";
                 echo "<h5 class='d-md-none'>Add Student: <i>".$fairNameYear."</i></h5>";
 
                 echo "<script type='text/javascript'>";
 
-                //echo "function formValidation() { var title = document.addForm.projTitle;if (title == '') {window.alert( 'Please provide Project Title!' );projTitle.focus();return false;}}";
+                
 
                 echo "</script>";
 
-			          echo "</head>";
-			          echo "</div>";
-			          echo "<br /><br />";
-			          echo "<div class='container'>";
+                echo "</head>";
+                echo "</div>";
+                echo "<br /><br />";
+                echo "<div class='container'>";
+        
                 
-                // onsubmit='return formValidation();'
-			          echo "<form name='addform' method='POST' action='addStudent.php?id=".$ID."'>";
-		
-			          // Project title
-			          echo "<div class='form-row'>";
-			          echo "<div class='form-group col'>";
-			          echo "<label>Project Title</label>";
-			          echo "<input type='text' class='form-control' id='projTitle' name='projTitle' placeholder='Project Title'>";
-			          echo "</div>";
-			          echo "</div>";
+                echo "<form method='POST' action='practiceForm.php?id=".$ID."'>";
 
-			          // first and last name
-  			        echo "<div class='form-row'>";
-  			        echo "<div class='form-group col'>";
-  	 		        echo "<label>First Name:</label>";
-    		        echo "<input type='text' class='form-control' id='fname' name='fname' placeholder='First name'>";
-    		        echo "</div>";
-    		        echo "<div class='form-group col'>";
-			          echo "<label>Last Name:</label>";
-    		        echo "<input type='text' class='form-control' id='lname' name='lname' placeholder='Last name'>";
-    		        echo "</div>";
-  			        echo "</div>";
+                
 
-  			        // Age and Gender
-  			        echo "<div class='form-row'>";
-  			        echo "<div class='form-group col-6'";
-  			        echo "<label>Age:</label>";
-  			        echo "<input type='number' class='form-control' value='12' id='age' name='age'>";
-  			        echo "</div>";
-  			        echo "<div class='form-check form-check-inline'>";
-  			        echo "<input class='form-check-input' type='radio' name='genderRadioOptions' id='inlineRadio1' value='Male'>";
-  			        echo "<label class='form-check-label' for='inlineRadio1'>Male</label>";
-  			        echo "</div>";
-  			        echo "<div class='form-check form-check-inline'>";
-  			        echo "<input class='form-check-input' type='radio' name='genderRadioOptions' id='inlineRadio2' value='Female'>";
-  			        echo "<label class='form-check-label' for='inlineRadio2'>Female</label>";
-  			        echo "</div>";
-  			        echo "</div>";
+                // Project title
+                echo "<div class='form-row'>";
+                echo "<div class='form-group col'>";
+                echo "<label>Project Title</label>";
+                echo "<input type='text' class='form-control' id='projTitle' name='projTitle' placeholder='Project Title'>";
+                echo "</div>";
+                echo "</div>";
 
-  			        // city, state, and zip code
-  			        echo "<div class='form-row'>";
-  			        echo "<div class='form-group col'>";
-  			        echo "<label>City:</label>";
-  			        echo "<input type='text' class='form-control' id='city' name='city' placeholder='City'>";
-  			        echo "</div>";
-  			        echo "<div class='form-group col'>";
-  			        echo "<label>State Abbreviation (e.g. MS):</label>";
-  			        echo "<input type='text' class='form-control' id='state' name='state' placeholder='State'>";
-  			        echo "</div>";
-  			        echo "<div class='form-group col'>";
-  			        echo "<label>Zip Code:</label>";
-  			        echo "<input type='number' class='form-control' id='zip' name='zip' placeholder='12345'>";
-  			        echo "</div>";
-  			        echo "</div>";
+                // first and last name
+                echo "<div class='form-row'>";
+                echo "<div class='form-group col'>";
+                echo "<label>First Name:</label>";
+                echo "<input type='text' class='form-control' id='fname' name='fname' placeholder='First name'>";
+                echo "</div>";
+                echo "<div class='form-group col'>";
+                echo "<label>Last Name:</label>";
+                echo "<input type='text' class='form-control' id='lname' name='lname' placeholder='Last name'>";
+                echo "</div>";
+                echo "</div>";
+
+                // Age and Gender
+                echo "<div class='form-row'>";
+                echo "<div class='form-group col-6'";
+                echo "<label>Age:</label>";
+                echo "<input type='number' class='form-control' value='12' id='age' name='age'>";
+                echo "</div>";
+                echo "<div class='form-check form-check-inline'>";
+                echo "<input class='form-check-input' type='radio' name='genderRadioOptions' id='inlineRadio1' value='Male'>";
+                echo "<label class='form-check-label' for='inlineRadio1'>Male</label>";
+                echo "</div>";
+                echo "<div class='form-check form-check-inline'>";
+                echo "<input class='form-check-input' type='radio' name='genderRadioOptions' id='inlineRadio2' value='Female'>";
+                echo "<label class='form-check-label' for='inlineRadio2'>Female</label>";
+                echo "</div>";
+                echo "</div>";
+
+                // city, state, and zip code
+                echo "<div class='form-row'>";
+                echo "<div class='form-group col'>";
+                echo "<label>City:</label>";
+                echo "<input type='text' class='form-control' id='city' name='city' placeholder='City'>";
+                echo "</div>";
+                echo "<div class='form-group col'>";
+                echo "<label>State Abbreviation (e.g. MS):</label>";
+                echo "<input type='text' class='form-control' id='state' name='state' placeholder='State'>";
+                echo "</div>";
+                echo "<div class='form-group col'>";
+                echo "<label>Zip Code:</label>";
+                echo "<input type='number' class='form-control' id='zip' name='zip' placeholder='12345'>";
+                echo "</div>";
+                echo "</div>";
 
                 // Adult Sponsor
                 echo "<div class='form-row'>";
@@ -147,28 +174,28 @@
                 echo "<input type='text' class='form-control' id='adultSponsor' name='adultSponsor' placeholder='Name'>";
                 echo "</div>";
                 echo "</div>";
-		
-  			        // select school
-  			        $query = "Select * from School order by SName";
-  			        //echo "<h2>GOT HERE</h2>";
-  			        $result = $mysqli->query($query);
-  			        if ($result && $result->num_rows > 0) {
+        
+                // select school
+                $query = "Select * from School order by SName";
+                //echo "<h2>GOT HERE</h2>";
+                $result = $mysqli->query($query);
+                if ($result && $result->num_rows > 0) {
 
-  				          echo "<div class='form-row'>";
-  				          echo "<div class='form-group col'>";
-  				          echo "<label>School:</label>";
-  				          echo "<select class='form-control' id='school' name='school'>";
+                    echo "<div class='form-row'>";
+                    echo "<div class='form-group col'>";
+                    echo "<label>School:</label>";
+                    echo "<select class='form-control' id='school' name='school'>";
                     echo "<option>Choose Option</option>";
-  				          while ($row = $result->fetch_assoc()) {
-  					            echo "<option value='".$row['SchoolID']."'>".$row['SName']."</option>";
-  				          }
-  				          echo "</select>";
-  				          echo "</div>";
-  				          echo "</div>";
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<option value='".$row['SchoolID']."'>".$row['SName']."</option>";
+                    }
+                    echo "</select>";
+                    echo "</div>";
+                    echo "</div>";
 
-  			        } else {
-  				          echo "<h2>Schools not Available</h2>";
-  			        }
+                } else {
+                    echo "<h2>Schools not Available</h2>";
+                }
 
                 // category
                 $query = "Select * from Category";
@@ -212,10 +239,11 @@
                     echo "<h2>Class not Available</h2>";
                 }
 
-  			        echo "<br />";
-  			        echo "<button type='submit' name='submit' class='btn btn-primary btn-block'>Submit</button>";
-			          echo "</form>";
-			          echo "</div>";
+                echo "<br />";
+                echo "<button type='submit' name='submit' class='btn btn-primary btn-block'>Submit</button>";
+                echo "</form>";
+                echo "</div>";
+
             } else {
                 $_SESSION["message"] = "Error! Fair could not be found.";
                 header("Location: SFindex.php");
@@ -226,9 +254,9 @@
             header("Location: SFindex.php");
             exit;
         }
-		}
-		
-	 
+    }
+        
+     
  ?>
  <br /><br /><br />
  </html>
