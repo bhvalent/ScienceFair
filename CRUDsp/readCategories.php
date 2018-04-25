197f8b23db6a99f62a9d7b12f97468b20ee8096d
bennettval <benhudvalentine@gmail.com>
2018-04-25 01:08:05 -0500

    added export button

--- CRUDsp/readCategories.php
+++ CRUDsp/readCategories.php
@@ -71,6 +71,17 @@
 				echo "</div>";
 				echo "</div>";
 
+				// Export button
+				echo "<br /><br />";
+				echo "<div class='row justify-content-center' align='center'>";
+				echo "<div class='col-xs-12 col-sm-12 col-md-9 col-lg-6'>";
+				echo "<p><b>Export Schools</b></p>";
+				echo "<p></p>";
+				echo "<a href='exportCategories.php?id=".$ID."' class='btn btn-success btn-block'>Export</a>";
+				echo "</div>";
+				echo "</div>";
+				echo "<br /><br /><br /><br /><br /><br />";
+
 
 			} else {
 				$_SESSION["message"] = "Error! Categories were not found.";
