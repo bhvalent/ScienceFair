d61b33115b3bc2cf04778945cc5d5d8c2e54b5ec
bennettval <benhudvalentine@gmail.com>
2018-05-07 02:22:25 -0500

    syntax error

--- ScienceFair3.sql
+++ ScienceFair3.sql
@@ -45,8 +45,8 @@ CREATE TABLE JudgeRegistrant (
 -- Table: Login
 CREATE TABLE Login (
     Username varchar(25) NOT NULL,
-    Password varchar(100) NOT NULL,
-    CONSTRAINT Login_pk PRIMARY KEY (Username,Password)
+    Pword varchar(100) NOT NULL,
+    CONSTRAINT Login_pk PRIMARY KEY (Username,Pword)
 );
 
 -- Table: Registration
